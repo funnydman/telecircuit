15bb7cdf6ac1cdfcc63d9ab471ae022e52a210ac
FUNNYDMAN <crazy-dzmitry@mail.ru>
2018-06-21 21:32:41 +0300

    add pagination to page

--- circuits/views.py
+++ circuits/views.py
@@ -10,4 +10,17 @@ def circuits(request):
 def get_circuits_list(request, brand):
     brand = apps.get_model('circuits', brand.title())
     models = brand.objects.all()
-    return render(request, 'circuits.html', {"models": models})
+
+    paginator = Paginator(models, 5)  # Show 5 pag_items per page
+
+    page = request.GET.get('page')
+    try:
+        items_per_page = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        items_per_page = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        items_per_page = paginator.page(paginator.num_pages)
+
+    return render(request, 'circuits.html', {"models": models, "items_per_page": items_per_page})
