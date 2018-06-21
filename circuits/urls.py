3e81265290352870b2e42685bd8f1bc7e96b394b
FUNNYDMAN <crazy-dzmitry@mail.ru>
2018-06-21 19:35:54 +0300

    add correct paths to circuits page

--- circuits/urls.py
+++ circuits/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('circuits/', views.get_circuits_list, name='circuits-list')
+    path('circuits/', views.get_circuits_list, name='circuits')
 ]
