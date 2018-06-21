4333b06a703b581142660bf5a8838d446197721f
FUNNYDMAN <crazy-dzmitry@mail.ru>
2018-06-21 20:31:08 +0300

    add view for displaying model data in the template

--- circuits/urls.py
+++ circuits/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('circuits/', views.get_circuits_list, name='circuits')
+    path('circuits/', views.circuits, name='circuits'),
+    path('circuits/<str:brand>/', views.get_circuits_list, name="circuits-list")
 ]
--- circuits/views.py
+++ circuits/views.py
@@ -1,35 +1,13 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, redirect
-
+from django.apps import apps
 from orders.forms import ContactForm
-from .models import Samsung
+
+def circuits(request):
+    return render(request, 'circuits.html', {})
 
 
-def get_circuits_list(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            email = form.cleaned_data['email']
-            message = form.cleaned_data['message']
-            form.save()
-            return redirect('circuits-list')
-    else:
-        form = ContactForm()
-
-    models_of_brand = Samsung.objects.all()
-
-    paginator = Paginator(models_of_brand, 3)  # Show 3 pag_items per page
-
-    page = request.GET.get('page')
-    try:
-        pag_items = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        pag_items = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        pag_items = paginator.page(paginator.num_pages)
-    return render(request, 'circuits.html',
-                  {'models_of_brand': models_of_brand, 'pag_items': pag_items,
-                   'form': form})
+def get_circuits_list(request, brand):
+    brand = apps.get_model('circuits', brand.title())
+    models = brand.objects.all()
+    return render(request, 'circuits.html', {"models": models})
